fea18608cfc39bc283f266ae6fa20b4b4c7ef4b8
Cody <cody.tian@icloud.com>
2022-07-21 16:38:20 -0700

    add nonworking JackTokenizer.tokenType()

--- jackAnalyzer.py
+++ jackAnalyzer.py
@@ -1,9 +1,12 @@
 from jackTokenizer import *
+from enum import Enum
+
 # from compilationEngine import *
 
-tokenizer = JackTokenizer('ExpressionLessSquare/Main.jack')
+tokenizer = JackTokenizer('ExpressionLessSquare/SquareGame.jack')
 for line in tokenizer.file:
     print(line + "|" + str(len(line)))
 
 while tokenizer.hasMoreTokens():
     tokenizer.advance()
+    print(tokenizer.token_type())
--- jackTokenizer.py
+++ jackTokenizer.py
@@ -1,3 +1,14 @@
+from enum import Enum
+
+
+class TokenType(Enum):
+    KEYWORD = 0
+    SYMBOL = 1
+    IDENTIFIER = 2
+    INT_CONST = 3
+    STRING_CONST = 4
+
+
 class JackTokenizer:
     def __init__(self, filename):
         self.file = []
@@ -11,9 +22,10 @@ class JackTokenizer:
                 line = line[:(line.index('/'))]
             except:
                 pass
-            line = line.strip(' ')
-            if (len(line) > 0) and (line[0] != '/'):
-                self.file.append(line[:-1])
+
+            if (len(line) > 1) and (line[0] != '/'):
+                line = line[:-1].strip(' ')
+                self.file.append(line)
 
     def hasMoreTokens(self):
         return not ((self.line_number >= len(self.file) - 1) and (
@@ -34,7 +46,8 @@ class JackTokenizer:
         except:
             pass
 
-        for token_breaker in ['+', '-', '~', '*', '/', '&', '|', '<', '>', '=', '}', '{', ')', '(', '[', ']', '.', ',', ';']:
+        for token_breaker in ['+', '-', '~', '*', '/', '&', '|', '<', '>', '=',
+                              '}', '{', ')', '(', '[', ']', '.', ',', ';']:
             try:
                 index = rest_of_line.index(token_breaker)
                 if index > 0:
@@ -45,12 +58,17 @@ class JackTokenizer:
                 pass
 
         if len(possible_token_ending_indices) > 1:
-            for i in range(0, len(possible_token_ending_indices)-1):
-                possible_token_ending_indices[0] = min(possible_token_ending_indices[0], possible_token_ending_indices[i+1])
+            for i in range(0, len(possible_token_ending_indices) - 1):
+                possible_token_ending_indices[0] = min(
+                    possible_token_ending_indices[0],
+                    possible_token_ending_indices[i + 1])
 
         token_end = possible_token_ending_indices[0]
 
         self.current_token = rest_of_line[:token_end]
+
+        # if self.current_token[0] == "\"":
+        #     self.current_token = rest_of_line[:rest_of_line.index("\"") + 1]
 
         # for now, just advance for every word
         # but first, we have to find the starting line index of our token which will be our self.currentIndex.
@@ -66,3 +84,23 @@ class JackTokenizer:
                 self.current_token = self.current_token[:-1]
 
         print(f'Current token: |{self.current_token}|')
+
+    def token_type(self):
+        if self.current_token in ['class', 'constructor', 'function', 'method',
+                                  'field', 'static', 'var', 'int', 'char',
+                                  'boolean', 'void', 'true', 'false', 'null',
+                                  'this', 'let', 'do', 'if', 'else', 'while',
+                                  'return']:
+            return TokenType.KEYWORD
+        if self.current_token in ['{', '}', '(', ')', '[', ']', '.', ',', ';',
+                                  '+', '-', '*', '/', '&', '|', '<', '>', '=',
+                                  '~']:
+            return TokenType.SYMBOL
+        if self.current_token[0] in ['0', '1', '2', '3', '4', '5', '6', '7',
+                                     '8', '9']:
+            return TokenType.INT_CONST
+        if self.current_token[0] == ["\""]:
+            self.advance()  # like this we're skipping the first token (") so that the next token i our actual string constant
+            return TokenType.STRING_CONST
+        else:
+            return TokenType.IDENTIFIER
