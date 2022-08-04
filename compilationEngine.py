3e9e6d7c3145079bd2e01c43e3d6aee0f260caa9
Cody <cody.tian@icloud.com>
2022-08-03 18:09:19 -0700

    add compile_class_var_dec(), compile_identifier(), with no compile_type()

--- compilationEngine.py
+++ compilationEngine.py
@@ -10,11 +10,24 @@ class CompilationEngine:
     # advances a token and checks if it is the correct one, if given a list sees if it is in the list
     def checkToken(self, *token_and_type):
         self.jack_tokenizer.advance()
+
+        wrote = False
         for token in token_and_type:
             if self.jack_tokenizer.current_token == token[0]:
                 self.output.write(
                     '<' + token[1] + '>' + token[0] + '</' + token[1] + '>\n'
                 )
+                wrote = True
+
+        if not wrote:
+            raise ValueError('The current token is incorrect.')
+
+    def compile_identifier(self):
+        if self.jack_tokenizer.token_type() == TokenType.IDENTIFIER:
+            self.output.write(
+                '<identifier>' + self.jack_tokenizer.current_token + '</identifier>\n')
+        else:
+            raise ValueError('The current token is incorrect.')
 
     def compile_class(self):
         self.output.write(
@@ -23,9 +36,7 @@ class CompilationEngine:
 
         self.checkToken(['class', 'keyword'])
         self.jack_tokenizer.advance()
-        if self.jack_tokenizer.token_type() == TokenType.IDENTIFIER:
-            self.output.write(
-                '<identifier>' + self.jack_tokenizer.current_token + '</identifier>\n')
+        self.compile_identifier()
 
         self.checkToken(['{', 'symbol'])
         while True:
@@ -36,11 +47,33 @@ class CompilationEngine:
 
         while True:
             try:
-                self.compile_subroutine_var_dec()
+                self.compile_subroutine_dec()
             except:
                 break
         self.checkToken(['}', 'symbol'])
 
         self.output.write(
             '</class>'
+        )
+
+    def compile_class_var_dec(self):
+        self.output.write(
+            '<classVarDec>\n'
+        )
+
+        self.checkToken(['static', 'keyword'], ['field', 'keyword'])
+        self.compile_type()
+        self.compile_identifier()
+
+        while True:
+            try:
+                self.checkToken([',', 'symbol'])
+                self.compile_identifier()
+            except:
+                break
+
+        self.checkToken([';', 'symbol'])
+
+        self.output.write(
+            '</classVarDec>'
         )
