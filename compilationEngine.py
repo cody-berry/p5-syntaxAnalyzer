50fc1a846963e38d0d33b8fa97694d2eb77de668
Cody <cody.tian@icloud.com>
2022-10-03 13:35:09 -0700

    add CompilationEngine.compile_type()

--- compilationEngine.py
+++ compilationEngine.py
@@ -16,7 +16,7 @@ class CompilationEngine:
             print(self.tokenizer.current_token + '|')
 
     # advances a token and checks if it is the correct one,
-    # if given a list sees if it is in the list
+    # if given a list sees if it is in the list of tokens, literally 'tokens'
     def check_token(self, advance, tokens):
         if advance:
             self.advance()
@@ -110,6 +110,33 @@ class CompilationEngine:
 
         self.indents -= 1
         self.output.write('  </classVarDec>\n')
+
+    # grammar: 'int', 'boolean', 'char', or an identifier. sometimes you'll
+    # want to advance, other times you won't. for example, if you just called
+    # check_token() or compile_identifier(), you'll want to advance. if you
+    # just did an advance() or you just finished an asterisk loop or question
+    # if-else statement.
+    def compile_type(self, advance=True):
+        for i in range(0, self.indents):
+            # two spaces as a substitute for a tab. useful for testing purposes
+            # because all test cases use two spaces for indentations too.
+            self.output.write('  ')
+
+        self.output.write('<classVarDec>\n')
+        self.indents += 1
+
+        # advance if we want to advance
+        if advance:
+            self.advance()
+
+        # 'int', 'boolean', or 'char' with the if-else statement
+        if self.tokenizer.current_token in ['int', 'boolean', 'char']:
+            self.check_token(False, ['int', 'boolean', 'char'])
+        else:  # and otherwise it's className, an identifier.
+            self.compile_identifier(False)
+
+        self.indents -= 1
+        self.output.write('</classVarDec>\n')
 
     # grammar: constructor/function/method void/type subroutineName
     # '(' parameterList ')' subroutineBody
